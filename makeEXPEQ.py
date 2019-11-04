ed4d0b9ac94cdd90f7306409dc428d30b3cd76b5
victowne <weit@vip.qq.com>
2019-11-04 22:45:27 +0800

    finished

--- makeEXPEQ.py
+++ makeEXPEQ.py
@@ -5,6 +5,7 @@ NWBPS = 2 # =1,only plasma; =2 plasma&wall
 NDATA = 2 # =2,uniform resistivity
 NPPF1 = 201 # grid number of the profiles
 NSTTP = 2 # =1,TT';=2,toroidal current density;=3,parallel current density
+
 #Specify Plasma Boundary#
 #Dee Formula
 rmax = 0.43
@@ -20,4 +21,34 @@ Z2 = eshape * rmax * np.sin(tdum)
 epsilon = rmax/rzero
 
 #Profiles
-s = 
+s = np.linspace(0,1,NPPF1) # s = sqrt((psi-psi_axis)/(psi_edge-psi_axis))
+dpdr = -12*s*(1-s**2)**5
+q = s**3 + s**2 + 0.95
+ds2 = (s[1] - s[0])*2
+dqdr = np.ones(NPPF1)
+for i in range(1,NPPF1-1):
+    dqdr[i] = (q[i+1] - q[i-1])/ds2
+dqdr[0] = 1.33333333*dqdr[1] - 0.33333333*dqdr[2] 
+dqdr[NPPF1-1] = 1.33333333*dqdr[NPPF1-2] - 0.33333333*dqdr[NPPF1-3]
+dqdr = 3*s**2 + 2*s
+cur = (2-s/q*dqdr)/q
+
+#Write file
+f = open('EXPEQ.test','w')
+f.write('  %.16e\n' %epsilon)
+f.write('  %.16e\n' %0.)
+f.write('  %.16e\n' %0.)
+f.write(' %d    %d    %d\n' %(nbound,NWBPS,NDATA))
+for i in range(0,nbound):
+    f.write('  %.16e %.16e\n' %(R[i],Z[i]))
+for i in range(0,nbound):
+    f.write('  %.16e %.16e\n' %(R2[i],Z2[i]))
+f.write(' %d\n' %NPPF1)
+f.write('    %d\n' %NSTTP)
+for i in range(0,NPPF1):
+    f.write('  %.16e\n' %s[i])
+for i in range(0,NPPF1):
+    f.write('  %.16e\n' %dpdr[i])
+for i in range(0,NPPF1):
+    f.write('  %.16e\n' %cur[i])
+f.close()
